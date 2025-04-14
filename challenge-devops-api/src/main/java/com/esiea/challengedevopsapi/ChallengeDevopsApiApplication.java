d67e44962307f19fc59e814cd0743b101ca6a1dd
4Kosmoos <firmin.shu@gmail.com>
2025-04-14 09:32:49 +0200

    first-commit

---
+++ challenge-devops-api/src/main/java/com/esiea/challengedevopsapi/ChallengeDevopsApiApplication.java
@@ -0,0 +1,13 @@
+package com.esiea.challengedevopsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeDevopsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeDevopsApiApplication.class, args);
+    }
+
+}
---
+++ challenge-devops-api/src/test/java/com/esiea/challengedevopsapi/ChallengeDevopsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esiea.challengedevopsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeDevopsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
